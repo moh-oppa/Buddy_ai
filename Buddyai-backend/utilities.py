9db03c1c011212a40bb1f3fbc7e5be32772df2de
moh-oppa <fasholakorede01@gmail.com>
2026-05-11 23:09:16 +0100

    ...

--- Buddyai-backend/utilities.py
+++ Buddyai-backend/utilities.py
@@ -4,6 +4,8 @@ from fastapi import File, HTTPException, UploadFile
 
 MAX_TEXT_LENGTH = 80000
 
+
+# parsing pdf
 async def parse_pdf(doc: UploadFile = File(...)):
     try:
         content = await doc.read()
@@ -17,6 +19,7 @@ async def parse_pdf(doc: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=422, detail=f"Error processing file: {str(e)}")
 
+
 async def parse_text(doc: UploadFile = File(...)):
     try:
         content = await doc.read()
@@ -26,6 +29,8 @@ async def parse_text(doc: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=422, detail=f"Error processing file: {str(e)}")
 
+
+# parsing dox
 async def parse_docx(doc: UploadFile = File(...)):
     try:
         from docx import Document as DocxDocument
