78e676d116d2064198e37601aa55f4eb3a5863ed
moh-oppa <fasholakorede01@gmail.com>
2026-08-27 22:32:26 +0100

    ..

--- Buddyai-backend/main.py
+++ Buddyai-backend/main.py
@@ -211,6 +211,7 @@ async def chat(request: Request, body: ChatRequest, doc_id: str, db: Session = D
 @app.post("/buddyai/extract/{doc_id}", response_model=ExtractResponse)
 @limiter.limit("30/minute")
 async def extract(request: Request, doc_id: str, db: Session = Depends(get_db)):
+    #ggnow
     docs = db.query(DocumentModel).filter(DocumentModel.id == doc_id).first()
     if not docs:
         raise HTTPException(status_code=404, detail="Document not found!")
